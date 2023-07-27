e8fc91e32c5962c469e72c6c48760b5996d41c43
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2023-07-27 18:13:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use(serverRouter)
 
+// Route for the admin subdomain
+app.get('/admin', (req, res) => {
+  // Logic to serve your admin dashboard (e.g., admin.html) here
+  res.sendFile(path.join(__dirname, '../admin/index.html'));
+});
+
 app.listen(port,()=>{
     console.log("server is up and running on ",port)
 })
