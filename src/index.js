a0ffea8cca091904608170a089ac60fe39a8a442
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2023-07-27 18:43:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ app.use(express.json())
 app.use(serverRouter)
 
 // Route for the admin subdomain
-app.get('/admin', (req, res) => {
+app.get('/', (req, res) => {
    if (req.subdomains.length > 0 && req.subdomains[0] === 'admin') {
   // Logic to serve your admin dashboard (e.g., admin.html) here
-       return res.redirect('https://results-4p8w-admin.onrender.com');
+      console.log(req.subdomains);
+       res.redirect('https://results-4p8w-admin.onrender.com');
       //res.sendFile(path.join(__dirname, '../admin'));
    } else {
     // If accessed directly without the "admin" subdomain, respond with an error message or redirect
