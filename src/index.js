d311af8d1c0f7da34942d01cd112a2e0a702ea8a
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2023-07-27 18:21:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,14 @@ app.use(serverRouter)
 
 // Route for the admin subdomain
 app.get('/admin', (req, res) => {
+   if (req.subdomains.length > 0 && req.subdomains[0] === 'admin') {
   // Logic to serve your admin dashboard (e.g., admin.html) here
-  res.sendFile(path.join(__dirname, '../admin/index.html'));
+      res.sendFile(path.join(__dirname, '../admin'));
+   } else {
+    // If accessed directly without the "admin" subdomain, respond with an error message or redirect
+    res.status(403).send('Access to the admin dashboard is not allowed.');
+    // or res.redirect('https://yourdomain.com'); // Redirect to the main domain, for example
+  }
 });
 
 app.listen(port,()=>{
