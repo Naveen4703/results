f6d58950231565d8e68abc80dd7df4535109a9d1
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2023-07-27 20:25:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
    if (req.subdomains.length > 0 && req.subdomains[0] === 'admin') {
   // Logic to serve your admin dashboard (e.g., admin.html) here
       console.log(req.subdomains);
-       res.redirect('https://results-4p8w-admin.onrender.com');
-      //res.sendFile(path.join(__dirname, '../admin'));
+      // res.redirect('https://results-4p8w-admin.onrender.com');
+      res.sendFile(path.join(__dirname, '../admin'));
    } else {
     // If accessed directly without the "admin" subdomain, respond with an error message or redirect
     res.status(403).send('Access to the admin dashboard is not allowed.');
